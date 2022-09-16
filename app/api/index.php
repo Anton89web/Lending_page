bc7efb516d5fa230da5bafe40f328f2709b7be80
Anton <fenix-live@mail.ru>
2022-09-16 15:58:40 +0300

    add classes components

---
+++ app/api/index.php
@@ -0,0 +1,3 @@
+<?php
+  echo "<h1>PHP work</h1>";
+?>
--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,31 @@
-import $ from "jquery";
+import React from "react";
+import ReactDom from "react-dom";
+import {createRoot} from "react-dom/client";
+import Editor from "./components/editor";
 
-$("h1").fadeOut();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Editor);
+
+// ReactDom.render(<Editor/>, document.getElementById('root'));
+
+// function getPageList() {
+//   $("h1").remove();
+//   $.get("./api", data => {
+//     data.forEach(file => {
+//       $("body").append(`<h1>${file}</h1>h1>`)
+//     })
+//   },"JSON");
+// }
+//
+// getPageList();
+//
+// $("button").on("click", function (){
+//   $.post("./api/createNewPage.php", {
+//     "name": $("input").val()
+//   }, () => {
+//     getPageList();
+//   })
+//     .fail(()=>{
+//       alert('Страница уже существует!');
+//     })
+// });
